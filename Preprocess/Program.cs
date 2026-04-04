d572ac469fba8c3fbf9fe587d2ea5573b5512fb7
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-04-04 09:35:17 +0300

    added npy reading

--- Preprocess/Program.cs
+++ Preprocess/Program.cs
@@ -1,9 +1,19 @@
 using Microsoft.Extensions.Configuration;
+using NumSharp;
 
 public static class Preprocess
 {
     public static void Run(IConfigurationRoot config)
     {
-        Console.WriteLine("Running preprocessing...");
+        var datasetPath = config["DatasetPath"]
+            ?? throw new InvalidOperationException("DatasetPath is not configured");
+
+        var firstFile = Directory.EnumerateFiles(Path.Combine(datasetPath, "new_joint_vecs"), "*.npy")
+            .Order()
+            .First();
+
+        var data = np.Load<float[,]>(firstFile);
+        Console.WriteLine($"Loaded {firstFile}: shape=({data.GetLength(0)}, {data.GetLength(1)})");
+        Console.WriteLine($"First frame (first 5 values): [{string.Join(", ", Enumerable.Range(0, 5).Select(i => data[0, i]))}]");
     }
 }
