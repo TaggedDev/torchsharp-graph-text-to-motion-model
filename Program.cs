c9aefe23216cf332c73bb9716018551827ba35b6
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-04-04 08:52:19 +0300

    Added run configuration

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
-﻿namespace text_to_motion;
+﻿using Microsoft.Extensions.Configuration;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string mode = args.Length > 0 ? args[0] : string.Empty;
+        if (!ValidateMode(mode))
+            return;
+
+        string configFileName = $"{mode}.json";
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile($"Configs/{configFileName}", optional: false, reloadOnChange: true)
+            .Build();
+
+        Console.WriteLine($"Running mode={mode}");
+
+        mode switch
+        {
+            "preprocessing" => Preprocess.Run(config),
+            "training" => Train.Run(config),
+            "inference" => Inference.Run(config),
+            _ => throw new ArgumentException("Mode is invalid but should have been caught before.")
+        };
+    }
+
+    private static bool ValidateMode(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            throw new ArgumentException("Mode is required");
+        return s switch
+        {
+            "preprocessing" or "training" or "inference" => true,
+            _ => throw new ArgumentException(
+                $"Mode is invalid: {s}. Valid values are 'preprocessing', 'training', 'inference'.")
+        };
     }
 }
