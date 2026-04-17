3bcec389c0eef9614608b460a577e798949a826d
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-04-17 16:05:30 +0300

    Added model loading

--- MotionModel/TrainingSettings.cs
+++ MotionModel/TrainingSettings.cs
@@ -3,4 +3,6 @@ namespace Text2Motion.TorchTrainer;
 public class TrainingSettings
 {
     public string OutputRootPath { get; set; } = string.Empty;
+    public bool LoadCheckpoint { get; set; }
+    public int LoadRunNumber { get; set; }
 }
