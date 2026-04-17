da3292054b4b40ddc759cf6c0a9f8a103a8fb450
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-04-17 12:32:42 +0300

    Added metrics logs

---
+++ MotionModel/TrainerMetricsLog.cs
@@ -0,0 +1,13 @@
+﻿namespace Text2Motion.TorchTrainer;
+
+public class TrainerMetricsLog
+{
+    public List<int> Epochs { get; set; } = [];
+    public List<float> TrainLoss { get; set; } = [];
+    public List<float> ValidationLoss { get; set; } = [];
+    public List<float> TestLoss { get; set; } = [];
+    public List<float> TrainMetrics { get; set; } = [];
+    public List<float> ValidationMetrics { get; set; } = [];
+    public List<float> TestMetrics { get; set; } = [];
+    public List<float> EpochSeconds { get; set; } = [];
+}
