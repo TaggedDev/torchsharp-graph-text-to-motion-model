e8199114b48751d487c25625087acde5e1b8923d
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-04-22 08:20:31 +0300

    Implemented GCN model + config

--- ModelConfigs/GcnSpatialTemporalConfig.cs
+++ ModelConfigs/GcnSpatialTemporalConfig.cs
@@ -2,5 +2,10 @@
 
 public class GcnSpatialTemporalConfig
 {
-    
+    public int JointFeatureDim { get; set; } = 64;
+    public int NumGcnLayers { get; set; } = 3;
+    public int NumTemporalLayers { get; set; } = 2;
+    public int TemporalKernelSize { get; set; } = 3;
+    public int TemporalPadding { get; set; } = 1;
+    public float DropoutRate { get; set; } = 0.1f;
 }
